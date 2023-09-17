edb09a11f126058241454d65a343283511323c5f
Ruslan <batoca2019@gmail.com>
2023-09-17 14:16:19 +0800

    lifetwig pom login v1, working version

--- src/test/java/pages/lifetwig/LTLoginPage.java
+++ src/test/java/pages/lifetwig/LTLoginPage.java
@@ -1,7 +1,6 @@
 package pages.lifetwig;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+
 
 import pages.BasePage;
 
@@ -10,7 +9,7 @@ public class LTLoginPage extends BasePage {
     private static final String Login_Page = "https://www.lifetwig.com/login";
     private static final String Unauthorized_login = "ruslan.axle@gmail.com";
     private static final String Login_input = "id=login_email";
-    private static final String Unauthorizen_password= "Penguin25!";
+    private static final String Unauthorizedpassword = "Penguin25!";
     private static final String Password_input = "id=login_password";
     private static final String Submit_button= "xpath=//button[@type='submit']";
     private static final String Invalid_Password_Error = "xpath=//a[contains(@href,'/user-profile')]";
@@ -23,7 +22,7 @@ public class LTLoginPage extends BasePage {
     public void fillOutCredentials() {
 
         steps.typeIn(Unauthorized_login, Login_input);
-        steps.typeIn(Unauthorizen_password, Password_input);
+        steps.typeIn(Unauthorizedpassword, Password_input);
         steps.clickTo(Submit_button);
 
     }
